3e6a5b9db36ae0b840d9a3c64eef3f96f5e7ba3e
Bohdan Korogodsky <bogdankorogodskij@gmail.com>
2024-01-22 14:16:27 +0300

    +

--- hello.c
+++ hello.c
@@ -3,13 +3,16 @@
 int main(void)
 {
     int a = 10;
-    int b = 23;
     int *pa = &a;
-    int *pb = &b;
-    int c = pa - pb;
+    printf("pa: address=%p \t value=%d \n", (void *)pa, *pa);
+    int b = (*pa)++;
 
-    printf("pa=%p \n", (void *)pa);
-    printf("pb=%p \n", (void *)pb);
-    printf("c=%d \n", c);
+    printf("b: value=%d \n", b);
+    printf("pa: address=%p \t value=%d \n", (void *)pa, *pa);
+
+    printf("b => %d \n", b);
+    
+    printf("a => %d \n", a);
+
     return 0;
 }
