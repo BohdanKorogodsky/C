6d3fc17e1964cfdac932c7c3bbd185531fb217e7
Bohdan Korogodsky <bogdankorogodskij@gmail.com>
2024-01-22 15:59:07 +0300

    +

--- hello.c
+++ hello.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-// void swap(int *a, int *b);
+void swap(int *a, int *b);
 
 void swap(int *pa, int *pb) {
     int t = *pa;
     *pa = *pb;
-    *pa = t;
+    *pb = t;
     printf("*pa = %d \t *pb = %d \n", *pa, *pb);
 }
 
