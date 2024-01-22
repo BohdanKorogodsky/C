3b6e88d7c41344f155724cbc081e1d87aa13f8c3
Bohdan Korogodsky <bogdankorogodskij@gmail.com>
2024-01-22 13:58:16 +0300

    +

--- hello.c
+++ hello.c
@@ -5,13 +5,13 @@ int main(void)
     double d = 10.6;
     double *pd = &d;
     printf("Pointer pd: address=%p \n", (void *)pd);
-    pd++;
+    pd = pd + 2;
     printf("Pointer pd: address=%p \n", (void *)pd);
 
     char c = 'N';
     char *pc = &c;
     printf("Pointer pc: address=%p \n", (void *)pc);
-    pc++;
+    pc = pc - 3;
     printf("Pointer pc: address=%p \n", (void *)pc);
     
     return 0;
