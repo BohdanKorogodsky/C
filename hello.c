3af6e728fa3c22619b774bbffb3301c4a8414db6
Bohdan Korogodsky <bogdankorogodskij@gmail.com>
2024-01-22 15:20:29 +0300

    +

--- hello.c
+++ hello.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
-
-int main(void)
-{
-    int a = 10;
-    int *pa = &a;
-    printf("pa: address=%p \t value=%d \n", (void *)pa, *pa);
-    int b = ++*pa;
-
-    printf("b: value=%d \n", b);
-    printf("pa: address=%p \t value=%d \n", (void *)pa, *pa);
-
-    printf("b => %d \n", b);
-    
-    printf("a => %d \n", a);
-
-    return 0;
+#include <stdlib.h>
+int main(void) {
+    if (printf("%s\n", "Hello, world!") == -1) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
